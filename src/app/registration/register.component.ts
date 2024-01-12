3d8e4cb6cc73e4b8bcbb09ad95ef2428715a6832
Nemanja Stjepanović <nemanjastjepanovic222@gmail.com>
2024-01-12 03:18:52 +0100

    feat: added validation for registration

--- src/app/registration/register.component.ts
+++ src/app/registration/register.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {AuthService} from "../authentication/auth.service";
 import {Router} from "@angular/router";
 import {RegistrationService} from "./registration.service";
@@ -20,16 +20,58 @@ export class RegisterComponent implements OnInit{
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(3)]],
       confirmPassword: ['', Validators.required],
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      address: ['', Validators.required],
-      phoneNumber: ['', Validators.required],
+      firstName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+$/)]],
+      lastName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+$/)]],
+      address: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9\s]+$/)]],
+      phoneNumber: ['', [Validators.required, Validators.pattern(/^[0-9]{7,10}$/)]],
       userType: ['', Validators.required]
     });
   }
 
+  passwordsNotMatch(): boolean {
+    const password = this.registrationForm.get('password')?.value;
+    const confirmPassword = this.registrationForm.get('confirmPassword')?.value;
+
+    return password !== confirmPassword;
+  }
+  checkField(fieldName: string): string | null {
+    const control: AbstractControl | null = this.registrationForm.get(fieldName);
+
+    if (control?.hasError('email')) {
+      return `Invalid email address! `;
+    }
+    if (control?.hasError('pattern')) {
+      if (fieldName === 'firstName') {
+        return `First name should have only letters!.`;
+      } else if (fieldName === 'lastName') {
+        return `First name should have only letters!`;
+      } else if (fieldName === 'phoneNumber') {
+        return `Phone number should have only numbers! `;
+      } else {
+        return `Wrong format for ${fieldName}.`;
+      }
+    }
+    if (fieldName === 'confirmPassword' && this.passwordsNotMatch()) {
+      return `Passwords should match! .`;
+    }
+    return null;
+  }
+
   onSubmit(): void {
-    if (this.registrationForm.valid) {
+
+    const errors: string[] = [];
+    Object.keys(this.registrationForm.controls).forEach(key => {
+      const errorMessage = this.checkField(key);
+      if (errorMessage) {
+        errors.push(errorMessage);
+      }
+    });
+
+    if (errors.length > 0) {
+      for (const error of errors) {
+        alert(error);
+      }
+    } else {
       const registration : Registration = {
         email: this.registrationForm.value.email,
         password: this.registrationForm.value.password,
