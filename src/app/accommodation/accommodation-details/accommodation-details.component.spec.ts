9149bfe983b5ef3e47afd7f1a16849a1de616319
OgnjenGligoric <gligoric383@gmail.com>
2024-01-22 17:49:52 +0100

    feat: tests passed

--- src/app/accommodation/accommodation-details/accommodation-details.component.spec.ts
+++ src/app/accommodation/accommodation-details/accommodation-details.component.spec.ts
@@ -1,9 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from "@angular/router";
 import { of } from 'rxjs';
-import { MatDialog, MAT_DIALOG_SCROLL_STRATEGY, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import {MatDialog, MAT_DIALOG_SCROLL_STRATEGY, MAT_DIALOG_DATA, MatDialogModule} from "@angular/material/dialog";
 import { OverlayModule, Overlay } from '@angular/cdk/overlay';
 import { OverlayPositionBuilder } from '@angular/cdk/overlay';
 import { ViewportRuler } from '@angular/cdk/scrolling';
@@ -17,15 +17,32 @@ import { AuthService } from "../../authentication/auth.service";
 import { NotificationService } from "../../notifications/notification.service";
 import { DatePipe } from "@angular/common";
 import {Injector} from "@angular/core";
+import { BrowserModule, By } from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SharedModule } from '../../shared/shared.module';
+
+
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+
 
 describe('AccommodationDetailsComponent', () => {
   let component: AccommodationDetailsComponent;
   let fixture: ComponentFixture<AccommodationDetailsComponent>;
-
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let authService: AuthService;
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [AccommodationDetailsComponent],
-      imports: [HttpClientTestingModule, CommonModule, OverlayModule],
+      imports: [
+        HttpClientTestingModule,
+        CommonModule,
+        OverlayModule,
+        MatDialogModule,
+        NgbModule,
+        SharedModule],
       providers: [
         DatePipe,
         AccommodationService,
@@ -34,20 +51,14 @@ describe('AccommodationDetailsComponent', () => {
         AuthService,
         NotificationService,
         { provide: ActivatedRoute, useValue: { snapshot: { paramMap: { get: () => 'your-expected-param-value' } } } },
-        {
-          provide: MatDialog,
-          useValue: {
-            open: (component: ComponentType<any>, config?: any) => {},
-          },
-        },
-        { provide: MAT_DIALOG_DATA, useValue: {} },
-        { provide: MAT_DIALOG_SCROLL_STRATEGY, useValue: (overlay: Overlay, injector: Injector) => {} },
-      ],
+        ],
     }).compileComponents();
-
     fixture = TestBed.createComponent(AccommodationDetailsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    authService = TestBed.inject(AuthService);
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController)
+    // fixture.detectChanges();
   });
 
   it('should create', () => {
